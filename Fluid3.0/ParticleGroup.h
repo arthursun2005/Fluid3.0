719e01d7d41e0e506868d12ca40522cca41d83c0
Arthur Sun <arthur@ARTHUR-MAC.local>
2018-08-07 07:58:52 +1000

    first commit

---
+++ Fluid3.0/ParticleGroup.h
@@ -0,0 +1,13 @@
+//
+//  ParticleGroup.h
+//  Fluid3.0
+//
+//  Created by Arthur Sun on 8/6/18.
+//  Copyright © 2018 Arthur Sun. All rights reserved.
+//
+
+#ifndef ParticleGroup_h
+#define ParticleGroup_h
+
+
+#endif /* ParticleGroup_h */
---
+++ Fluid3.0/ParticleSystem.h
@@ -0,0 +1,13 @@
+//
+//  ParticleSystem.h
+//  Fluid3.0
+//
+//  Created by Arthur Sun on 8/5/18.
+//  Copyright © 2018 Arthur Sun. All rights reserved.
+//
+
+#ifndef ParticleSystem_h
+#define ParticleSystem_h
+
+
+#endif /* ParticleSystem_h */
