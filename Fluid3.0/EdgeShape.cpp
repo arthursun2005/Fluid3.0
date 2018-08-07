548e2e5731a38efd7ade79b0221b672d8c2c538f
Arthur Sun <arthur@ARTHUR-MAC.local>
2018-08-07 17:39:16 +1000

    EdgeShape

---
+++ Fluid3.0/EdgeShape.cpp
@@ -0,0 +1,9 @@
+//
+//  EdgeShape.cpp
+//  Fluid3.0
+//
+//  Created by Arthur Sun on 8/7/18.
+//  Copyright © 2018 Arthur Sun. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Fluid3.0/EdgeShape.h
@@ -0,0 +1,13 @@
+//
+//  EdgeShape.h
+//  Fluid3.0
+//
+//  Created by Arthur Sun on 8/7/18.
+//  Copyright © 2018 Arthur Sun. All rights reserved.
+//
+
+#ifndef EdgeShape_h
+#define EdgeShape_h
+
+
+#endif /* EdgeShape_h */
